a1070e2a68d18e57215fae4d883933ca7198d842
abed7helmi <abed.helmi@yahoo.fr>
2024-09-26 22:15:08 +0200

    init project

---
+++ src/main/java/net/helmi/springobservabilitygrafana/SpringObservabilityGrafanaApplication.java
@@ -0,0 +1,13 @@
+package net.helmi.springobservabilitygrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringObservabilityGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringObservabilityGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/helmi/springobservabilitygrafana/SpringObservabilityGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.helmi.springobservabilitygrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringObservabilityGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
